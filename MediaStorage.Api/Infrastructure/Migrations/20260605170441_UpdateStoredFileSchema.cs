2b47314d3910f09e0e6eb67bbfcd0577be27be4b
Duki <dukilicious@hotmail.com>
2026-06-05 19:05:12 +0200

    feat: update media storage database schema

---
+++ MediaStorage.Api/Infrastructure/Migrations/20260605170441_UpdateStoredFileSchema.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediaStorage.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateStoredFileSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
